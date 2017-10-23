f3946b5c0d9b9b9ae47d43e84cac5b1d96f7fe38
dylan <527844046@qq.com>
2017-10-23 11:14:17 +0800

    test

--- resources/views/chat/chatbox/index.blade.php
+++ resources/views/chat/chatbox/index.blade.php
@@ -36,19 +36,6 @@
             </div>
             <div class="H-chatbox-img H-position-relative"><img src="/images/me.jpg" class="H-display-block H-border-radius-circle" alt="" title="" /></div>
         </div>
-        <div class="H-chatbox-sender H-flexbox-horizontal H-padding-horizontal-both-10 H-box-sizing-border-box H-margin-vertical-top-10">
-            <div class="H-chatbox-main H-flex-item H-flexbox-horizontal H-position-relative H-margin-horizontal-right-12">
-                <div class="H-chatbox-status H-flex-item H-padding-horizontal-both-10 H-box-sizing-border-box H-text-align-right H-padding-vertical-top-12"></div>
-                <div class="H-chatbox-content">
-                    <div class="H-font-size-12 H-theme-font-color-444 H-padding-2 H-text-align-right">Hui官方<span class="H-display-inline-block H-padding-horizontal-both-5 H-theme-font-color-white H-theme-background-color8 H-margin-horizontal-left-5 H-line-height-normal H-border-radius-5 H-font-size-12">VIP5</span></div>
-                    <div class="H-position-relative">
-                        <div class="H-chatbox-content-text H-font-size-16 H-padding-10 H-theme-background-color1 H-theme-font-color-white H-border-radius-12">嗨，我也有名称哦。</div>
-                        <div class="H-chatbox-bugle H-theme-border-color1 H-position-absolute H-z-index-100 H-bugle-right"></div>
-                    </div>
-                </div>
-            </div>
-            <div class="H-chatbox-img H-position-relative"><img src="/images/me.jpg" class="H-display-block H-border-radius-circle" alt="" title="" /></div>
-        </div>
     </div>
     <script src="{{asset('Hui/js/H.js')}}" type="text/javascript"></script>
     <script type="text/javascript">
